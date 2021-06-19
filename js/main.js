f6828a06a59690318b1f17a828508f612a688a78
alanalaveszefino <alanalveszefino1999@unesc.net>
2021-06-19 01:18:51 -0300

    api

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ var ul = document.querySelector("#listaGames")
 var form = document.querySelector("#formCadastro")
 var inputName = document.querySelector("#name")
 var inputLink = document.querySelector("#link")
+var inputImg = document.querySelector("#img")
 form.addEventListener('submit' , Cadastro)
+
+
 
 function Cadastro(evento){
     evento.preventDefault()
     let game = {
         "name": inputName.value,
-        "link": inputLink.value
+        "link": inputLink.value,
+        "img" : inputImg.value
     }
     
     let texto = JSON.stringify(game)
@@ -25,7 +29,6 @@ function Cadastro(evento){
     }
     const requisicao = fetch(url, opcoes)
     requisicao.then(function(resposta){
-        console.log(resposta.statusText)
         console.log(resposta.status)
         recebeGames()
     })
@@ -48,8 +51,9 @@ function deletaGames(evento){
 function imprimeGames(){
     ul.textContent = ''
     for(const game of listaGames){
+
         const li = document.createElement('li')
-        li.textContent = `${game.name} (${game.link})`
+        li.textContent = `${game.img} ${game.name} (${game.link})`
         
         const botao = document.createElement('button')
         botao.textContent = "Desativar"
