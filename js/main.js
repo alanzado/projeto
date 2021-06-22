12a6da81d612fcd729baaae327ad4df8f75234b5
alanalaveszefino <alanalveszefino1999@unesc.net>
2021-06-22 20:47:45 -0300

    style

--- js/main.js
+++ js/main.js
@@ -1,87 +1,100 @@
-var listaGames = []
+var listaGames = [];
 
-const url = 'https://projeto-final-ppw.herokuapp.com/api/112135'
-var section = document.querySelector("#listaGames")
-var form = document.querySelector("#formCadastro")
-var inputName = document.querySelector("#name")
-var inputLink = document.querySelector("#link")
-var inputPhoto = document.querySelector("#photo")
-var inputData = document.querySelector("#data")
-var inputTag = document.querySelector("#tag")
-form.addEventListener('submit' , Cadastro)
+const url = "https://projeto-final-ppw.herokuapp.com/api/112135";
+var section = document.querySelector("#listaGames");
+var form = document.querySelector("#formCadastro");
+var inputName = document.querySelector("#name");
+var inputLink = document.querySelector("#link");
+var inputPhoto = document.querySelector("#photo");
+var inputData = document.querySelector("#data");
+var inputTag = document.querySelector("#tag");
+var inputDesc = document.querySelector("#desc");
+form.addEventListener("submit", Enviar);
 
-function Cadastro(evento){
-    evento.preventDefault()
+function Enviar(evento) {
+    evento.preventDefault();
     let game = {
-        "name": inputName.value,
-        "link": inputLink.value,
-        "photo" : inputPhoto.value,
-        "data" : inputData.value,
-        "tag" : inputTag.value
-        
-    }
-    
-    let texto = JSON.stringify(game)
+        name: inputName.value,
+        link: inputLink.value,
+        photo: inputPhoto.value,
+        data: inputData.value,
+        tag: inputTag.value,
+        desc: inputDesc.value,
+    };
+
+    let texto = JSON.stringify(game);
 
     const opcoes = {
         method: "POST",
         body: texto,
         headers: {
-            "content-type": "application/json"
-        }
-    }
-    const requisicao = fetch(url, opcoes)
-    requisicao.then(function(resposta){
-        console.log(resposta.status)
-        recebeGames()
-    })
+            "content-type": "application/json",
+        },
+    };
+    const requisicao = fetch(url, opcoes);
+    requisicao.then(function (resposta) {
+        console.log(resposta.status);
+        recebeGames();
+    });
 }
 
-function deletaGames(evento){
-    const id = evento.target.value
+function deletaGames(evento) {
+    const id = evento.target.value;
     const opcoes = {
-        "method": "DELETE"
-    }
+        method: "DELETE",
+    };
 
-    const requisicao = fetch(url + "/" + id, opcoes)
-    requisicao.then(function(resposta){
-        if(resposta.status == 200){
-            recebeGames()
+    const requisicao = fetch(url + "/" + id, opcoes);
+    requisicao.then(function (resposta) {
+        if (resposta.status == 200) {
+            recebeGames();
         }
-    })
+    });
 }
 
-function imprimeGames(){
-    section.textContent = ''
-    for(const game of listaGames){
-
-        const div = document.createElement('div')
-        div.innerHTML =`<div class="game">
+function imprimeGames() {
+    section.textContent = "";
+    for (const game of listaGames) {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="game">
         <img class="game-photo" src="${game.photo}">
         <h2 class="game-name">${game.name} <span class="tag">(${game.tag})</span></h2>
-        <p><strong>Ano:</strong> ${game.data}</p>
-        <a class="link" href="${game.link}" target="_blank">Resgatar</a>
+        <p><strong>Lançamento:</strong> ${age(game.data)}</p>
+        <p><strong>Desconto:</strong> ${game.desc}</p>
         </div>
-        `  
-        const botao = document.createElement('button')
-        botao.textContent = "Desativar"
-        botao.value = game._id
-        botao.onclick = deletaGames
+        `;
+        const botao = document.createElement("button");
+        botao.textContent = "Desativar";
+        botao.value = game._id;
+        botao.onclick = deletaGames;
 
-        div.appendChild(botao)
-        section.appendChild(div)
+        div.appendChild(botao);
+        section.appendChild(div);
     }
 }
 
-function recebeGames(){
-    const requisicao = fetch(url)
-    requisicao.then( function(resposta){
-        const json = resposta.json()
-        json.then(function(games){
-            listaGames = games
-            imprimeGames()
-        })
-    })
+function age(data) {
+    let calculatedAge = new Date().getFullYear() - data;
+    if (calculatedAge == 1) {
+        return "á 1 ano";
+    } else if (calculatedAge == 0) {
+        return "Novo";
+    } else {
+        return `á ${calculatedAge} anos`;
+    }
 }
 
-recebeGames()
+function recebeGames() {
+    const requisicao = fetch(url);
+    requisicao.then(function (resposta) {
+        const json = resposta.json();
+        json.then(function (games) {
+            listaGames = games;
+            imprimeGames();
+        });
+    });
+}
+
+recebeGames();
+
